28cd6940f6771d516b64adad9db886bba5529d53
jsk2319 <31487832+jsk2319@users.noreply.github.com>
2017-12-10 14:33:44 -0500

    Add files via upload

---
+++ DayDriver.java
@@ -0,0 +1,10 @@
+public class DayDriver {
+
+    public static void main(String[] args) {
+        Day day = new Day("Mon");
+        System.out.println("Current Day: " + day);
+        System.out.println("Next Day: " + day.nextDay());
+        System.out.println("Previous Day: " + day.previousDay());
+        System.out.println("The Next Four Days " + day.calculateDay(4));
+    }
+}
---
+++ ModulatorDemo.java
@@ -0,0 +1,13 @@
+public class ModulatorDemo {
+
+    public static void main(String[] args) {
+        System.out.println(0%7);
+        System.out.println(1%7);
+        System.out.println(2%7);
+        System.out.println(3%7);
+        System.out.println(4%7);
+        System.out.println(5%7);
+        System.out.println(6%7);
+        System.out.println(Math.abs((-8+7)%7));
+    }
+}
